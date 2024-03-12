766586952cf179bf440b79cf93cb49cbe3a8b23b
mateo <mateo.lostanlen@gmail.com>
2024-03-12 13:44:05 +0100

    missing embed dl

--- dl_tasks.py
+++ dl_tasks.py
@@ -8,11 +8,16 @@ import shutil
 import json
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from PIL import Image
 
 
 def download_file(s3_client, bucket_name, s3_object_key, local_file_path):
     try:
         s3_client.download_file(bucket_name, s3_object_key, local_file_path)
+        # Resize images
+        if local_file_path[-3:] == "jpg":
+            im = Image.open(local_file_path).resize((1280, 720))
+            im.save(local_file_path)
     except botocore.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "404":
             print(f"The file {s3_object_key} does not exist.")
@@ -23,12 +28,18 @@ def download_file(s3_client, bucket_name, s3_object_key, local_file_path):
 def download_folder(s3, bucket_name, prefix, local_dir):
     keys_to_download = []
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    os.makedirs(os.path.join(local_dir, "embeddings"), exist_ok=True)
     if "Contents" in response:
         for obj in response["Contents"]:
             s3_object_key = obj["Key"]
             local_file_path = os.path.join(local_dir, s3_object_key)
             os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
             keys_to_download.append((s3_object_key, local_file_path))
+            # Download embeddings
+            name = os.path.basename(s3_object_key).replace("jpg", "pth")
+            embed_s3_object_key = os.path.join("embeddings", name)
+            embed_local_file_path = os.path.join("data/embeddings", name)
+            keys_to_download.append((embed_s3_object_key, embed_local_file_path))
 
     with ThreadPoolExecutor(max_workers=12) as executor:
         future_to_key = {
