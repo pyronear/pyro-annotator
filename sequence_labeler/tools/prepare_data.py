4b9d5d6301874282678ae900cf8f505baab82490
Chouffe <arthur@caillau.me>
2025-07-21 16:09:52 +0200

    feat: move sequence_labeler to its own folder
    Also use uv properly

--- sequence_labeler/tools/prepare_data.py
+++ sequence_labeler/tools/prepare_data.py
@@ -5,7 +5,7 @@ import pathlib
 from multiprocessing import Pool, cpu_count
 
 from tqdm import tqdm
-from utils import generate_candidate_gifs, prepare_candidates_with_context
+from sequence_labeler.utils import generate_candidate_gifs, prepare_candidates_with_context
 
 # ✏️ Update this to your root folder of all sequences
 all_sequences_folder_path = "../arthur/pyro-dataset/data/raw/pyronear-platform/sequences/sis-67_test2/sis-67"
