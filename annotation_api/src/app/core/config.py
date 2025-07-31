5b20e7622a816dc11965f78ebf1e929158e7097f
Chouffe <arthur@caillau.me>
2025-07-31 11:34:15 +0200

    fix(config): typo in pyro-annotator title

--- annotation_api/src/app/core/config.py
+++ annotation_api/src/app/core/config.py
@@ -15,7 +15,7 @@ __all__ = ["settings"]
 
 class Settings(BaseSettings):
     # State
-    PROJECT_NAME: str = "pyro-annoator - Annotation API"
+    PROJECT_NAME: str = "pyro-annotator - Annotation API"
     PROJECT_DESCRIPTION: str = "API for managing platform annotations"
     VERSION: str = "0.1.0"
     API_V1_STR: str = "/api/v1"
